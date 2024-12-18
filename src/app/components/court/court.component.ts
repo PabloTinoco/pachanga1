47a1c27e34bcc96aa43a3652550c8423197ac685
Pablo Tinoco <pablo.tinoco@soltel.es>
2024-12-18 18:52:47 +0100

    Validadores Court (incompleto)

--- src/app/components/court/court.component.ts
+++ src/app/components/court/court.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CourtService } from '../../services/court.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-court',
@@ -10,20 +11,31 @@ import { Router } from '@angular/router';
 })
 export class CourtComponent {
   courtForm: FormGroup;
+  countries: any[] = [];
+  cities: string[] = [];
 
   constructor(
     private fb: FormBuilder,
     private courtService: CourtService,
-    private router: Router
+    private router: Router,
+    private http: HttpClient
   ) {
     this.courtForm = this.fb.group({
-      name: ['', Validators.required],
+      name: ['', Validators.required, Validators.minLength(3)],
       country: ['', Validators.required],
       city: ['', Validators.required],
-      postalCode: ['', Validators.required],
+      postalCode: ['', Validators.required,Validators.pattern(/^\d{4,6}$/)],
       address: ['', Validators.required],
-      coordinateX: ['', Validators.required],
-      coordinateY: ['', Validators.required],
+      coordinateX: ['', Validators.required, this.coordinateValidator],
+      coordinateY: ['', Validators.required, this.coordinateValidator],
+    });
+
+    // Cargar lista de países
+    this.http.get('https://restcountries.com/v3.1/all').subscribe((countries: any) => {
+      this.countries = countries.map((c: any) => ({
+        name: c.name.common,
+        code: c.cca2,
+      }));
     });
   }
 
@@ -41,5 +53,26 @@ export class CourtComponent {
         },
       });
     }
+  }
+
+  onCountryChange(countryCode: string): void {
+    // Obtener ciudades según el país
+    this.http
+      .get(`https://wft-geo-db.p.rapidapi.com/v1/geo/countries/${countryCode}/cities`, {
+        headers: {
+          'X-RapidAPI-Key': 'YOUR_API_KEY',
+        },
+      })
+      .subscribe((response: any) => {
+        this.cities = response.data.map((city: any) => city.name);
+      });
+  }
+
+  coordinateValidator(control: any): { [key: string]: any } | null {
+    const coordinatePattern = /^-?\d+(\.\d+)?$/;
+    if (control.value && !coordinatePattern.test(control.value)) {
+      return { invalidCoordinate: true };
+    }
+    return null;
   }
 }
